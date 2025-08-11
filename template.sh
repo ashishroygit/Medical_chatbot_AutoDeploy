367e00a2d7f659ea1ba20695aa02d67e500b366f
Ashish Roy <ashishgithubprojects@gmail.com>
2025-08-11 15:52:29 -0400

    Folder Structure Added

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Creating Directory
+mkdir -p src
+mkdir -p research
+
+#Creating files
+touch scr/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+
+
+echo "Directory and files created successfully"
